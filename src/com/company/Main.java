6b49d06bee4591beba25d00d2713d1884aefd7e9
HotnotHD <de-kovalevs@yandex.ru>
2020-02-14 19:40:35 +0300

    1.0 without tests

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,20 @@
 package com.company;
 
-import javafx.util.Pair;
-
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
         adressBook adresses = new adressBook();
-        adresses.adds("Ковалевский", new Pair<>("Воронина 20","22"));
-        adresses.adds("Пушкарев", new Pair<>("Ленина 16","65"));
+        adresses.adds("Ковалевский", new Triplet("Воронина",22,15));
+        adresses.adds("Пушкарев", new Triplet("Ленина",15,15));
         adresses.delete("Ковалевский");
-        adresses.changeAdress("Пушкарев", new Pair<>("Ленина 26","13"));
-        System.out.println(adresses.vse);
+        adresses.changeAdress("Пушкарев", new Triplet("Ленина",13,32));
+        adresses.adds("Ковалевский", new Triplet("Ленина",22,15));
+        adresses.adds("Ыуыр", new Triplet("Пленина",22,15));
+        Triplet ss = new Triplet("ss",15,34);
         System.out.println(adresses.getAdress("Пушкарев"));
+        System.out.println(ss.getFlat());
+        System.out.println(adresses.whoLifeStreet("Ленина"));
+        System.out.println(adresses.whoLifeFlat(15));
     }
 }
--- src/com/company/class.java
+++ src/com/company/class.java
@@ -1,25 +1,80 @@
 package com.company;
 
-import javafx.util.Pair;
-
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 class adressBook {
-    HashMap<String, Pair<String,String>> vse = new HashMap<>();
-    void adds(String family, Pair<String, String> STnums){
+    HashMap<String, Triplet> vse = new HashMap<>();
+
+    void adds(String family, Triplet STnums){
         if (!vse.containsKey(family))
         vse.put(family,STnums);
     }
+
     void delete (String name){
-        vse.remove(name);
+        if (vse.containsKey(name)){
+            vse.remove(name);
+        } else throw new NullPointerException("Такой фамилии не существует");
     }
-    void changeAdress(String name, Pair<String, String> STnums){
+
+    void changeAdress(String name, Triplet STnums){
         if (vse.containsKey(name)){
             vse.remove(name);
             vse.put(name,STnums);
-        }
+        } else throw new NullPointerException("Такой фамилии не существует");
     }
-    Pair<String,String> getAdress(String name) {
-        return vse.get(name);
+
+    String getAdress(String name){
+        if (vse.containsKey(name)){
+            return vse.get(name).getStreet() + " " + vse.get(name).getStreetNum() + " " + vse.get(name).getFlat();
+        } else throw new NullPointerException("Такой фамилии не существует");
+    }
+
+    ArrayList<String> whoLifeStreet(String streetIn){
+        ArrayList<String> names = new ArrayList<>();
+        for (Map.Entry<String,Triplet> entry : vse.entrySet())
+        {
+            String name = entry.getKey();
+            Triplet value = entry.getValue();
+            if (value.getStreet().equals(streetIn)){
+                names.add(name);
+            }
+        }
+        return names;
+    }
+
+    ArrayList<String> whoLifeFlat(int flatIn){
+        ArrayList<String> names = new ArrayList<>();
+        for (Map.Entry<String,Triplet> entry : vse.entrySet())
+        {
+            String name = entry.getKey();
+            Triplet value = entry.getValue();
+            if (value.getFlat() == (flatIn)){
+                names.add(name);
+            }
+        }
+        return names;
+    }
+}
+
+class Triplet {
+    private String street;
+    private int house;
+    private int flat;
+
+    Triplet(String street, int streetNum, int flat){
+        this.flat = flat;
+        this.street = street;
+        this.house = streetNum;
+    }
+    String getStreet(){
+        return street;
+    }
+    int getStreetNum(){
+        return house;
+    }
+    int getFlat(){
+        return flat;
     }
 }
